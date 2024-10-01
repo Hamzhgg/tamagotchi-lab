826965ab210365718c525acd0c92270fa12a71bf
root <gharibehhamzh@gemail.com>
2024-10-01 16:38:51 +0300

    setup html and css for game

--- js/app.js
+++ js/app.js
@@ -1,13 +1,36 @@
-/*-------------------------------- Constants --------------------------------*/
+// 1) Define the required variables used to track the state of the game.
 
+// 2) Store cached element references.
+
+// 3) Upon loading, the game state should be initialized, and a function should 
+//    be called to render this game state.
+
+// 4) The state of the game should be rendered to the user.
+
+// 5) Handle the game over logic. 
+
+// 6) Handle each instance of a player clicking a button with the use of a 
+//    `handleClick()` function.
+
+// 7) Create reset functionality.
+
+/*-------------------------------- Constants --------------------------------*/
+const state = {
+    boredom: 0,
+    hunger: 0,
+    sleepiness: 0,
+};
 
 
 /*---------------------------- Variables (state) ----------------------------*/
-
+let timer;
+let gameOver;
 
 
 /*------------------------ Cached Element References ------------------------*/
-
+const boredomStatEl = document.getElementById('boredom-stat');
+const hungerStatEl = document.getElementById('hunger-stat');
+const sleepinessStatEl = document.getElementById('sleepiness-stat');
 
 
 /*-------------------------------- Functions --------------------------------*/
